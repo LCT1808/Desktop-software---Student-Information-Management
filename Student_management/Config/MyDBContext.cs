065337f977ec1085c257b554ad7cd1c370fa071d
LCT1808 <lecamtu.0308@gmail.com>
2023-12-16 15:59:09 +0700

    update

--- Student_management/Config/MyDBContext.cs
+++ Student_management/Config/MyDBContext.cs
@@ -11,7 +11,7 @@ namespace Student_management.Config
 {
     public class MyDBContext: DbContext
     {
-        public MyDBContext() : base("Data Source=sheep;Initial Catalog=Student_Management;Trusted_Connection=True;TrustServerCertificate=True") { }
+        public MyDBContext() : base("Data Source=DESKTOP-J1JFN5D\\MSSQLSERVER01;Initial Catalog= Student_Management;Trusted_Connection = True; TrustServerCertificate=True;User ID = lecamtu; Password=lecamtu") { }
         public DbSet<Account> Accounts { get; set; }
         public DbSet<LoginHistory> LoginHistories { get; set; }
         public DbSet<Student> Students { get; set; }
